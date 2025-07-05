61d6b59cbacdd0f76d5a6be53b28dc3c6cb9641c
oligEdu <egarcia_junior@hotmail.com>
2025-07-05 17:32:33 -0300

    Implementação da Classe Postagem, Service, Controller e Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Postagem } from './Postagem/entities/postagem.entity';
+import { PostagemModule } from './Postagem/postagem.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '2224',
+      database: 'db_blogpessoal',
+      entities: [Postagem],
+      synchronize: true,
+      logging: true,
+    }),
+    PostagemModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
